761eb0854ee5d7f1fba6d249df50dbd5dc2fdb30
Amit P Naik <amit.naik8103@gmail.com>
2025-08-17 10:59:19 +0530

    feat: filter tab

--- src/components/common/SelectInput.js
+++ src/components/common/SelectInput.js
@@ -1,33 +1,25 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const SelectInput = ({
-  name,
-  label,
-  onChange,
-  defaultOption,
-  value,
-  error,
-  options
-}) => {
+const SelectInput = ({ name, label, onChange, defaultOption, value, error, options }) => {
   return (
     <>
-      <label id={name}>{label}</label>
+      <label htmlFor={name}>{label}</label>
       <select
         id={name}
         name={name}
-        value={value}
+        value={value || ""}               
         onChange={onChange}
-        className="form-control"
+        className={`form-control ${error ? "is-invalid" : ""}`}
       >
         <option value="">{defaultOption}</option>
-        {options.map((option) => (
-          <option key={option.value} value={option.value}>
-            {option.text}
+        {options.map((opt) => (
+          <option key={opt.value} value={opt.value}>
+            {opt.text}
           </option>
         ))}
       </select>
-      {error && <div className="valid-feedback">{error}</div>}
+      {error && <div className="invalid-feedback">{error}</div>}
     </>
   );
 };
