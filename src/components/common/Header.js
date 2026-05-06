91118796363abd23d4cacb671ed2a70f1f9a54db
Amit P Naik <amit.naik8103@gmail.com>
2026-05-06 23:41:32 +0530

    refactor: Header to functional component & styling

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,35 +1,76 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react';
 import { NavLink, Link } from "react-router-dom";
 import LogoImg from "../../img/logo3.png";
 
-export default class Header extends Component {
-    render() {
-        const activeStyle = { color: "#F15B2A" };
-        // const { login, logout, userHasScopes } = this.props.auth;
-        return (
-            <nav className="navbar navbar-expand-lg bg-white fixed-top">
-                <Link to="/" className="navbar-brand" >Bhagavad Gita</Link>
-                <img className="img-fluid" src={LogoImg} alt="card-img" height={20} width={40} />
-                <button className="navbar-toggler" type="button" data-toggle="collapse"
-                    data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-                    aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse " id="navbarSupportedContent">
-                    <ul className="navbar-nav ml-auto navbar-right-top">
-                        <li className="nav-item">
-                            <Link to="/" className="nav-link" activestyle={activeStyle}>Home</Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link to="/about" className="nav-link" activestyle={activeStyle}>About</Link>
-                        </li>
-                        {/* <li className="nav-item">
-                            <Link onClick={isAuthenticated() ? logout : login} className="nav-link" activestyle={activeStyle}>  {isAuthenticated() ? "Log Out" : "Log In"}</Link>
-                        </li> */}
-                    </ul>
-                </div>
-            </nav>
+const navLinks = [
+    { to: "/",      label: "Home"  },
+    { to: "/about", label: "About" },
+];
 
-        );
-    }
-}
+const Header = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+    const [scrolled, setScrolled] = useState(false);
+
+    useEffect(() => {
+        const onScroll = () => setScrolled(window.scrollY > 10);
+        window.addEventListener("scroll", onScroll, { passive: true });
+        return () => window.removeEventListener("scroll", onScroll);
+    }, []);
+
+    return (
+        <nav className={`app-header${scrolled ? " app-header--scrolled" : ""}`}>
+            {/* Brand */}
+            <Link to="/" className="app-header-brand" onClick={() => setMenuOpen(false)}>
+                <img src={LogoImg} alt="logo" className="app-header-logo" />
+                <span className="app-header-brand-text">Bhagavad Gita</span>
+            </Link>
+
+            {/* Desktop nav */}
+            <ul className="app-header-nav">
+                {navLinks.map(({ to, label }) => (
+                    <li key={to}>
+                        <NavLink
+                            to={to}
+                            end
+                            className={({ isActive }) =>
+                                `app-header-link${isActive ? " app-header-link--active" : ""}`
+                            }
+                        >
+                            {label}
+                        </NavLink>
+                    </li>
+                ))}
+            </ul>
+
+            {/* Hamburger */}
+            <button
+                className={`app-header-hamburger${menuOpen ? " open" : ""}`}
+                onClick={() => setMenuOpen(v => !v)}
+                aria-label="Toggle menu"
+            >
+                <span /><span /><span />
+            </button>
+
+            {/* Mobile drawer */}
+            {menuOpen && (
+                <div className="app-header-drawer">
+                    {navLinks.map(({ to, label }) => (
+                        <NavLink
+                            key={to}
+                            to={to}
+                            end
+                            className={({ isActive }) =>
+                                `app-header-drawer-link${isActive ? " app-header-link--active" : ""}`
+                            }
+                            onClick={() => setMenuOpen(false)}
+                        >
+                            {label}
+                        </NavLink>
+                    ))}
+                </div>
+            )}
+        </nav>
+    );
+};
+
+export default Header;
